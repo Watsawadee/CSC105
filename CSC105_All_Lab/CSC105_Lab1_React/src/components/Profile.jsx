b660fb55e08ba59650485d1506a8a08bcabac89e
watsawadee <s.watsawadee@gmail.com>
2023-06-09 15:25:34 +0700

    Merge all files into one repo

---
+++ CSC105_All_Lab/CSC105_Lab1_React/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import "./Profile.css";
+function Profile() {
+    return (
+        <div className="layout">
+            <img src="assets/image1.jpg" alt="profile"></img>
+
+            <h1 className="name">Capybara</h1>
+            <hr></hr>
+        </div>
+    );
+}
+
+export default Profile;
